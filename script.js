d7293504ef7ccb5e018d4b64e372197f94d8b2b7
eswarprasadk <eswarprasadk@turito.com>
2023-07-09 23:11:55 +0530

    music control logic

---
+++ script.js
@@ -0,0 +1,67 @@
+const musicRef = document.querySelector("audio");
+const prevRef = document.getElementById("prev");
+const nextRef = document.getElementById("next");
+const playRef = document.getElementById("play");
+
+const artistRef = document.getElementById("artist");
+const titleRef = document.getElementById("title");
+const imgRef = document.querySelector("img");
+
+const musicArray = [
+  {
+    title: "Mix-1",
+    fileName: "jacinto-1",
+    artist: "Eswar Prasad Kona",
+  },
+  {
+    title: "Mix-2",
+    fileName: "jacinto-2",
+    artist: "Eswar Prasad Kona",
+  },
+  {
+    title: "Mix-3",
+    fileName: "jacinto-3",
+    artist: "Eswar Prasad Kona",
+  },
+  {
+    title: "Mix-4",
+    fileName: "metric-1",
+    artist: "Eswar Prasad Kona",
+  },
+];
+
+let isPlaying = false;
+let songIndex = 0;
+function toggleAudio() {
+  isPlaying ? musicRef.pause() : musicRef.play();
+  isPlaying
+    ? (playRef.classList.replace("fa-pause", "fa-play"),
+      playRef.setAttribute("title", "play"))
+    : (playRef.classList.replace("fa-play", "fa-pause"),
+      playRef.setAttribute("title", "pause"));
+  isPlaying = !isPlaying;
+}
+
+playRef.addEventListener("click", toggleAudio);
+prevRef.addEventListener("click", () => {
+  songIndex =
+    songIndex == 0 ? (songIndex = musicArray.length - 1) : songIndex - 1;
+  changeMusic(musicArray[songIndex]);
+  isPlaying = false;
+  toggleAudio();
+});
+nextRef.addEventListener("click", () => {
+  songIndex = songIndex == musicArray.length - 1 ? 0 : songIndex + 1;
+  changeMusic(musicArray[songIndex]);
+  isPlaying = false;
+  toggleAudio();
+});
+
+function changeMusic(song) {
+  titleRef.textContent = song.title;
+  artistRef.textContent = song.artist;
+  musicRef.src = `./music/${song.fileName}.mp3`;
+  imgRef.src = `./img/${song.fileName}.jpg`;
+}
+
+changeMusic(musicArray[songIndex]);
